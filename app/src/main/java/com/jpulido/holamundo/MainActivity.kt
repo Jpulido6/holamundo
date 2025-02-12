a656795020d827e6376d701e8dab5aa54bc1fb9a
Carlos Jose <cjpulido6@gmail.com>
2025-02-12 17:02:41 -0500

    Initial commit

---
+++ app/src/main/java/com/jpulido/holamundo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jpulido.holamundo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jpulido.holamundo.ui.theme.HolamundoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HolamundoTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    Greeting(Modifier.padding(innerPadding))
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting( modifier: Modifier = Modifier) {
+    Column (modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        Text("Hola mundo👋")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HolamundoTheme {
+        Greeting()
+    }
+}
